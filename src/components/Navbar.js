03f12745e5030042df030169cd598fcf9050c685
QC <cheng305@purdue.edu>
2019-01-24 15:06:13 -0500

    styled.components and react context

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../logo.svg';
+import styled from 'styled-components';
+import {ButtonContainer} from './Button';
 
 class Navbar extends Component {
     render() {
         return (
-            <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+            <NavWrapper className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
                 {/* https://www.iconfinder.com/icons/1243689/call_phone_icon
 Creative Commons (Attribution 3.0 Unported);
 https://www.iconfinder.com/Makoto_msk */}
@@ -18,15 +20,25 @@ https://www.iconfinder.com/Makoto_msk */}
                         <Link to="/" className="nav-link">Products</Link>
                     </li>
                 </ul>
-            <Link to="/cart" className="ml-auto"> 
-                <button>
-                    <i className="fas fa-cart-plus"/>
-                    my cart
-                </button>
-            </Link>
-            </nav>
+                <Link to="/cart" className="ml-auto">
+                    <ButtonContainer>
+                        <span className="mr-2">
+                            <i className="fas fa-cart-plus" />
+                        </span>
+                        my cart
+                </ButtonContainer>
+                </Link>
+            </NavWrapper >
         );
     }
 }
 
+const NavWrapper = styled.nav`
+    background: var(--mainBlue);
+    .nav-link {
+        color: var(--mainWhite) !important;
+        font-size: 1.3rem;
+        text-transform: capitalize;
+    }
+`
 export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import {BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { ProductProvider } from './context';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>
+    <ProductProvider>
+        <Router>
+            <App />
+        </Router>
+    </ProductProvider>
     , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
